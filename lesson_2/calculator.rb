a9fdd62bd6223d52eae192cdde094590445b995c
JaimeLynSchatz <jaimelynschatz@gmail.com>
2016-02-17 17:45:39 -0800

    next - rubocop calculator

--- lesson_2/calculator.rb
+++ lesson_2/calculator.rb
@@ -8,28 +8,96 @@
 # perform the operation on the two numbers
 # output the result
 
-puts "Wecome to Calculator!"
-
-puts "What's the first number?"
-number1 = gets.chomp
-puts "The number is #{number1}!"
-
-puts "what's the second number?"
-number2 = gets.chomp
-puts "The numer is #{number2}!"
-
-puts "What operation would you like to perform? 1) add 2) subtract 3) multiply 4) divide?"
-operator = gets.chomp
-
-if operator == '1'
-  result = number1.to_i + number2.to_i
-elsif operator == '2'
-  result = number1.to_i - number2.to_i
-elsif operator == '3'
-  result = number1.to_i * number2.to_i
-elsif operator == '4'
-  result = number1.to_f / number2.to_f
+def prompt(message)
+  puts "=> #{message}"
 end
 
-puts "The result is #{result}"
+def valid_number?(num)
+  num.to_i != 0
+end
 
+def operation_to_message(op)
+  case op
+  when '1'
+    'Adding'
+  when '2'
+    'Subtracting'
+  when '3'
+    'Multiplying'
+  when '4'
+    'Dividing'
+  end
+end
+
+prompt "Wecome to Calculator! Enter your name:"
+
+name = ''
+loop do
+  name = Kernel.gets.chomp
+
+  if name.empty?
+    prompt "Make sure to use a valid name."
+  else
+    prompt "Hi, #{name}"
+    break
+  end
+end
+
+stop = ''
+while stop != 'Y'
+  number1 = ''
+  loop do
+    prompt "What's the first number?"
+    number1 = gets.chomp
+
+    if valid_number? number1
+      prompt "The number is #{number1}!"
+      break
+    else
+      prompt "Hmm... that doesn't look like a valid number"
+    end
+  end
+
+  number2 = ''
+  loop do
+    prompt "what's the second number?"
+    number2 = gets.chomp
+
+    if valid_number? number2
+      prompt "The numer is #{number2}!"
+      break
+    else
+      prompt "Hmm... that doesn't look like a valid number"
+    end
+  end
+
+  operator = ''
+  prompt "What operation would you like to perform?\n\t1) add\n\t2) subtract\n\t3) multiply\n\t4) divide"
+  loop do
+    operator = gets.chomp
+
+    if %w(1 2 3 4).include? operator
+      break
+    else
+      prompt "Must choose 1, 2, 3 or 4"
+    end
+  end
+
+  prompt operation_to_message operator
+
+  result = case operator
+           when '1'
+             number1.to_i + number2.to_i
+           when '2'
+             number1.to_i - number2.to_i
+           when '3'
+             number1.to_i * number2.to_i
+           when '4'
+             number1.to_f / number2.to_f
+           end
+
+  puts "The result is #{result}"
+
+  prompt "Do you want to quit? (Y to quit)"
+  stop = gets.chomp
+end
