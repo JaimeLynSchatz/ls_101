46cf1b92fc70b3684836ab31f4b8b05a122b3283
JaimeLynSchatz <jaimelynschatz@gmail.com>
2016-02-19 11:57:14 -0800

    solved mortgage calculator

--- lesson_2/mortgage_calculator.rb
+++ lesson_2/mortgage_calculator.rb
@@ -23,30 +23,25 @@ def mortgage_calculator(loan_amount, apr, loan_duration)
   # P = loan_amount(c( 1 + c)**loan_duration*12)/((1 + c)**n - 1)
 
   monthly_rate = calculate_monthly_rate(apr)
-  puts "Monthly rate of #{monthly_rate} of #{apr} APR"
   n = calculate_n_months(loan_duration)
-  puts "#{n} months for #{loan_duration} years"
-  dividend = ( 1 + monthly_rate )**n -1
-  puts "1 + monthly_rate = #{1 + monthly_rate}"
-  puts "then to the #{n} power = #{(1 + monthly_rate)**n}"
-  puts "minus 1 = #{dividend}?"
-  #dividend = dividend**n - 1
-  divisor = monthly_rate * ( ( 1 + monthly_rate)**n - 1 )
-  #divisor = divisor**n - 1
-  #divisor = monthly_rate * divisor
-  p = loan_amount * divisor / dividend
-  # p = loan_amount( monthly_rate ( 1 + monthly_rate )**n ) / ( ( 1 + monthly_rate )**n - 1 )
 
+  wiki_payment = monthly_rate * loan_amount / (1 - (1 + monthly_rate)**-n)
   puts "For a loan of #{loan_amount}, an APR of #{apr} over #{loan_duration} years,"
-  puts "You'll make #{n} monthly payments of #{p} each"
+  puts "You'll make #{n} monthly payments of #{wiki_payment.round(2)} each"
 end
 
 puts "*****************"
 puts "\n"
-mortgage_calculator(400_000, 0.035, 30)
+mortgage_calculator(200_000, 0.065, 30)
 puts "\n"
 puts "*****************"
-#mortgage_calculator(100_000, 0.05, 20)
-#mortgage_calculator(200_000, 0.05, 20)
-#mortgage_calculator(100_000, 0.05, 10)
-#mortgage_calculator(100_000, 0.10, 20)
+mortgage_calculator(100_000, 0.05, 20)
+puts "\n"
+puts "*****************"
+mortgage_calculator(200_000, 0.05, 20)
+puts "\n"
+puts "*****************"
+mortgage_calculator(100_000, 0.05, 10)
+puts "\n"
+puts "*****************"
+mortgage_calculator(100_000, 0.10, 20)
