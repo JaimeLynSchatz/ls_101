21ea7fbac91fbe2f616180a30059d7e8ed100df1
Jaime Lyn Schatz <jaimelynschatz@gmail.com>
2016-02-21 23:16:35 -0800

    middle of variable shadowing

--- lesson_2/coding_tips_2.rb
+++ lesson_2/coding_tips_2.rb
@@ -1,7 +1,8 @@
 # coding_tips_2.rb
 
-# bad code new lines - hulk sad
+# The Art of Newlines
 
+#bad code new lines - hulk sad
 name = ''
 puts "Enter your name: "
 loop do
@@ -12,9 +13,7 @@ end
 puts "Welcome #{name}!"
 puts "What would you like to do?"
 
-
-# beter
-
+# better
 name = ''
 
 puts "Enter your name: "
@@ -26,4 +25,62 @@ end
 
 puts "Welcome #{name}!"
 puts "What would you like to do?"
+
+
+# To side effect or return or both
+# Lesson: stay away from both - just pick one
+
+# side effect: displays something to the output
+# returns: nil
+def total(num1, num2)
+  puts num1 + num2
+end
+
+# side effect: mutates the passed-in array
+# returns: updated array
+def append(target_array, value_to_append)
+  target_array << value_to_append
+end
+
+# side effect: none
+# returns: new integer
+def total(num1, num2)
+  num1 + num2
+end
+
+
+# Name methods appropriately
+
+def display_something
+  # make something pretty looking
+  # and display it
+end
+
+def verb
+  # do that verb
+end
+
+words = %w(scooby doo on channel two)
+words.each {|str| puts str}
+
+words = %w(scooby do on channel two)
+wors.each {|str| str << '!'}
+puts words.inspect
+
+# just don't mutate the collection while iterating
+# thigs get weird and unexpected
+words = %w(scooby doo on channel two)
+words.each {|str| words.delete(str)}
+puts words.inspect  # => ["doo", "channel"], not an empty array
+
+
+# variable shadowing
+name = 'johnson'
+
+['kim', 'joe', 'sam'].each do |name|
+  # uh oh, we cannot access "name"
+  puts "#{name} #{name}"
+end
+
+
 
