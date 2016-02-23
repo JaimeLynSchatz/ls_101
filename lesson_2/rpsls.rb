d5905f177dc79c06ad97cbbe7a1d2d30918d2d81
Jaime Lyn Schatz <jaimelynschatz@gmail.com>
2016-02-22 22:13:10 -0800

    rpsls abbrv works, trying to keep score now

--- lesson_2/rpsls.rb
+++ lesson_2/rpsls.rb
@@ -8,24 +8,26 @@ DEFEATS = {
   'spock' => %w(rock scissors)
 }.freeze
 
-# rps.rb
+ABBRV = {
+  'r' => 'rock',
+  'p' => 'paper',
+  's' => 'scissors',
+  'l' => 'lizard',
+  'sp' => 'spock'
+}.freeze
 
 VALID_CHOICES = %w(rock paper scissors lizard spock).freeze
-
-def test_method
-  prompt('test message')
-end
-
-# test_method
 
 def prompt(message)
   Kernel.puts("=> #{message}")
 end
 
-test_method
-
 def win?(first, second)
   DEFEATS[first].include? second
+end
+
+def increment_winner(winner_score)
+  winner_score += 1
 end
 
 def choose_winner(choice, computer_choice)
@@ -33,17 +35,21 @@ def choose_winner(choice, computer_choice)
     "It's a tie!"
   elsif win?(choice, computer_choice)
     "You won!"
+    increment_winner(user_score)
   else
     "You lost!"
+    increment_winner(computer_score)
   end
 end
 
 def user_choice
   choice = ''
+  user_score = 0
+  computer_score = 0
   loop do
     prompt("Choose one: #{VALID_CHOICES.join(', ')}")
     choice = Kernel.gets().chomp()
-    if VALID_CHOICES.include?(choice)
+    if VALID_CHOICES.include?(choice) || ABBRV.key?(choice)
       break
     else
       prompt("That's not a valid choice.")
@@ -54,11 +60,18 @@ end
 
 loop do
   choice = user_choice
+  choice = ABBRV[choice] if choice.length <= 2
   computer_choice = VALID_CHOICES.sample
 
   prompt("You chose: #{choice}; Computer chose: #{computer_choice}")
 
   prompt choose_winner(choice, computer_choice)
+
+  if user_score == 5
+    prompt("You five times!")
+  elsif computer_score == 5
+    prompt("Too bad, the computer won five times!")
+  end
 
   prompt("Would you like to play again?")
   play_again = gets.chomp
