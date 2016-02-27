a00d5bef8e9f677a0e35b08059099c08555b987d
Jaime Lyn Schatz <jaimelynschatz@gmail.com>
2016-02-27 11:36:38 -0800

    create lesson 4 dir, add rubocop yml file

---
+++ lesson_4/notes.rb
@@ -0,0 +1,8 @@
+# Slightly more complicated programs ... Bring 'em!
+
+# Break down the problem into smaller pieces
+# Map out the flow with a chart
+# Write out psudocode
+# Play around with the code! Don't just copy and paste
+# Do the assignments in sequence
+# Videos are ok, but don't just watch -- do!
