93bc580610dcb2bc2de468c4da45636a55e2aef8
JaimeLynSchatz <jaimelynschatz@gmail.com>
2016-03-03 16:44:21 -0800

    deal a hand, evaluate for aces

--- lesson_4/twenty-one.rb
+++ lesson_4/twenty-one.rb
@@ -12,6 +12,8 @@
 # card = { 'suit' => 'rank' }
 # new_deck = # array of cards
 
+ranks = { '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10, J: 10, Q: 10, K: 10, A: 1}
+
 def fresh_deck
   deck = [ 
     ['C', '2'], ['C', '3'], ['C', '4'], ['C', '5'], ['C', '6'], ['C', '7'], ['C', '8'], ['C', '9'], ['C', '10'], ['C', 'J'], ['C', 'Q'], ['C', 'K'], ['C', 'A'],
@@ -20,4 +22,20 @@ def fresh_deck
     ['S', '2'], ['S', '3'], ['S', '4'], ['S', '5'], ['S', '6'], ['S', '7'], ['S', '8'], ['S', '9'], ['S', '10'], ['S', 'J'], ['S', 'Q'], ['S', 'K'], ['S', 'A'],
   ]
 end
+
+hand = []
+deck = fresh_deck
+
+def deal_cards(deck, n, hand)
+  n.times { hand.push(deck.delete(deck.sample)) }
+end
+
+def add_cards(hand)
+  hand.each do
+    rank = card[1]
+  end
+end
+
+deal_cards(deck, 5, hand)
+
 
