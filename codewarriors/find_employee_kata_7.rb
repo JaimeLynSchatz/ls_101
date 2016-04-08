ccb84bc25f602894e8b17b6afc91a593ad46ac1e
Jaime Lyn Schatz <jaimelynschatz@gmail.com>
2016-04-08 07:37:17 -0700

    solved find employee role problem

--- codewarriors/find_employee_kata_7.rb
+++ codewarriors/find_employee_kata_7.rb
@@ -1,17 +1,29 @@
-You will be given an array of object literals holding the current employees of the company. You code must find the employee with the matching firstName and lastName and then return the role for that employee or if no employee is not found it should return "Does not work here!"
+# You will be given an array of object literals holding the current employees of the company. You code must find the employee with the matching firstName and lastName and then return the role for that employee or if no employee is not found it should return "Does not work here!"
 
-The array is preloaded and can be referenced using the variable employees ($employees in Ruby). It uses the following structure.
+# The array is preloaded and can be referenced using the variable employees ($employees in Ruby). It uses the following structure.
 
-employees = [ {'first_name'=> "Dipper", 'last_name'=> "Pines", 'role'=> "Boss"},
+$employees = [ 
+  {'first_name'=> "Dipper", 'last_name'=> "Pines", 'role'=> "Boss"},
+  {'first_name'=> "Dipper", 'last_name'=> "Peter", 'role'=> "Secretary"},
+  {'first_name'=> "Alan", 'last_name'=> "Turing", 'role'=> "Mathematician"},
+  {'first_name'=> "Ada", 'last_name'=> "Lovelace", 'role'=> "Computer Scientist"},
+  {'first_name'=> "Grace", 'last_name'=> "Hopper", 'role'=> "Compiler Inventor"},
+]
+
+
+
 
 
 def find_employees_role(name)
   #your code here, array is called $employees
-  return "Does not work here!" if $employees[name.split(' ').first] != name.split(' ')[1]
-  $employees
-  
+  result = $employees.select { |employee| name.split.first == employee['first_name'] && name.split.last == employee['last_name'] }
+  return "Does not work here!" if result.empty?
+  result.first['role']
 end
 
-Test.assert_equals(find_employees_role("Dipper Pines"), "Does not work here!")
-Test.assert_equals(find_employees_role("Morty Smith"), "Truck Driver")
-Test.assert_equals(find_employees_role("Anna Bell"), "Admin")
+p find_employees_role("Grace Hopper")
+p find_employees_role("Jaime Schatz")
+
+# assert_equals(find_employees_role("Dipper Pines"), "Does not work here!")
+# Test.assert_equals(find_employees_role("Morty Smith"), "Truck Driver")
+# Test.assert_equals(find_employees_role("Anna Bell"), "Admin")
